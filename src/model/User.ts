d241579607612046c4869d4ab76be80dd8a14465
Aditya Singh <adityaa9170843@gmail.com>
2025-05-09 12:27:02 +0530

    Add User schema with validation and message handling; include zod dependency

--- src/model/User.ts
+++ src/model/User.ts
@@ -21,6 +21,54 @@ const MessageSchema: Schema<Message> = new Schema ({
 
 export interface User extends Document {
     username:string;
+    email:string;
     password:string;
     verifyCode:string;
+    verifyCodeExpiry:Date;
+    isVerified:boolean;
+    isAcceptingMessages:boolean;
+    message:Message[];
 }
+
+const UserSChema: Schema<User> = new Schema ({
+   username:{
+        type: String,
+        required: [true, "Please enter a username"],
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter an email"],
+        unique: true,
+        match:[/.+@.+\..+/, "Please enter a valid email"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Please enter a code"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Please enter a code expiry"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAcceptingMessages: {
+        type: Boolean,
+        default: true,
+    },
+    message:{
+        type: [MessageSchema],
+        default: [],
+    }
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSChema ) 
+
+export default UserModel
