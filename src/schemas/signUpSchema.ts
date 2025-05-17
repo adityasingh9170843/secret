c27ccabb998a209ccc465235ded457820b9501ca
Aditya Singh <adityaa9170843@gmail.com>
2025-05-17 22:59:53 +0530

    fix: standardize formatting in acceptMessageSchema and signUpSchema files

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -12,5 +12,5 @@ export const signUpSchema = z.object({
     email: z.string().email({message:"Invalid email address"}),
     password: z.string()
     .min(8, { message: "Password must be at least 8 characters long" })
-    .max(32, { message: "Password must be at most 32 characters long" })
+    .max(32, { message: "Password must be at most 32 characters long" })  
 })
