19c6bee8c163bff711a92b0dbe5e9543e56777e4
Aditya Singh <adityaa9170843@gmail.com>
2025-11-09 11:34:54 +0530

    refactor: streamline GET function for fetching user message count

--- src/app/api/dashboard/analytics/route.ts
+++ src/app/api/dashboard/analytics/route.ts
@@ -1,73 +1,61 @@
-
 import UserModel from "@/model/User";
 import dbConnect from "@/lib/dbConnect";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/option";
 
-
-
-
-
-
-
 export async function GET() {
-    await dbConnect();
+  await dbConnect();
 
-    const session = await getServerSession(authOptions)
-    const username = session?.user.username
-    
-    if(!username){
-        return Response.json(
-            {
-                success: false,
-                message: "You must be logged in to access.",
-            },
-            {
-                status: 500,
-            }
-        );
-    }
+  const session = await getServerSession(authOptions);
+  const username = session?.user.username;
 
-    try{
-        const user = await UserModel.findOne({username})
-        if(!user){
-            return Response.json(
-                {
-                    success: false,
-                    message: "User not found.",
-                },
-                {
-                    status: 404,
-                }
-            );
-            
+  if (!username) {
+    return Response.json(
+      {
+        success: false,
+        message: "You must be logged in to access.",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found.",
+        },
+        {
+          status: 404,
         }
-        
-        const messageCount = user.message.length;
-        
- 
-        return Response.json(
-            {
-                success: true,
-                message: "Message count fetched successfully.",
-                messageCount,
-            },
-            {
-                status: 200,
-            }
-        );
-    }catch(e){
-        return Response.json(
-            {
-                success: false,
-                message: "Failed to fetch message count.",
-            },
-            {
-                status: 500,
-            }
-        );
+      );
     }
 
+    const messageCount = user.message.length;
 
-
+    return Response.json(
+      {
+        success: true,
+        message: "Message count fetched successfully.",
+        messageCount,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to fetch message count.",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
